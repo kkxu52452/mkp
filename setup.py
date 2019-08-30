9bf086e19a267374a1fe627844c0bd0c267f148d
kk Xu <kkxu52452@gmail.com>
2019-08-30 16:58:46 +0800

    start walking

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except:
     raise ImportError("You must have cython installed in order to use mkp!") 
 
 import os
-import numpy as np
+
 from distutils.core import setup, Extension
 
 
@@ -15,28 +15,6 @@ ext_modules += cythonize(Extension(
     include_dirs=['cpp/'],
     language='c++',
     extra_compile_args=["-std=c++1y"]))
-
-extentions_info = [
-    {
-        "cython_sourcefile": "python/mkp/_algorithms_cy/mthm.pyx"
-    }
-]
-
-include_path = [np.get_include(), 'cpp']
-
-for extention_info in extentions_info:
-    sourcefiles = extention_info.get("cpp_sourcefiles", [])
-    cython_sourcefile = extention_info.get("cython_sourcefile")
-    sourcefiles += [cython_sourcefile]
-    
-    # NOTE: extension name must match .pyx file name
-    extension_name = cython_sourcefile.replace('.pyx', '').replace(os.sep, '.')
-    ext_modules += cythonize(Extension(
-                                extension_name,
-                                sources=sourcefiles,
-                                include_dirs=include_path,
-                                language='c++',
-                                extra_compile_args=["-std=c++1y"]))
 
 
 setup(name='mkp',
@@ -51,6 +29,5 @@ setup(name='mkp',
           'mkp': 'python/mkp',
       },
       ext_modules=ext_modules,
-      install_requires=['numpy'],
-      include_dirs=[np.get_include(), 'cpp'])
+      )
 
