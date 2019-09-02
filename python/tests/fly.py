29a97f76ede93cf6a0d6cb473a53ed36e6eb4747
kk Xu <kkxu52452@gmail.com>
2019-09-02 19:38:24 +0800

    three capacity allocation

--- python/tests/fly.py
+++ python/tests/fly.py
@@ -6,14 +6,10 @@ import csv
 import os
 import shutil
 
-# p = [110, 150, 70, 80, 30, 5]
-# w = [40, 60, 30, 40, 20, 5]
-# c = [65, 85]
- 
-
-def generate_random_data(n, m, ra):
+def generate_dissimilar_data(n, m, ra):
     rng = np.random
-
+    rng.seed(30)
+    
     # weights
     w = rng.randint(1, ra, size=n)
     # profits
@@ -24,14 +20,62 @@ def generate_random_data(n, m, ra):
     upper_index = 0
     for i in range(m):
         if i < m - 1:
-            upper_index = rng.randint(w.size//(m + 1), w.size//m + 1) + lower_index
+            step = rng.randint(w.size//(m + 1), w.size//m + 1)
+            upper_index = step + lower_index
             c.append(w[lower_index:upper_index].sum())
         else:
             c.append(w[lower_index:].sum())
         lower_index = upper_index
     w = w.tolist()
     p = p.tolist()
-
+    print(c)
+    print(sum(c), sum(w))
+    return p, w, c
+    
+def generate_similar_data(n, m, ra):
+    rng = np.random
+    rng.seed(30)
+    
+    # weights
+    w = rng.randint(1, ra, size=n)
+    # profits
+    p = rng.randint(1, ra, size=n)
+    # capacities
+    c = []
+    w = w.tolist()
+    p = p.tolist()
+    low = 0.8*sum(w)//m
+    high = 1.2*sum(w)//m
+    for i in range(m):
+        c.append(rng.randint(low, high)) 
+    print(c)
+    print(sum(c), sum(w))
+    return p, w, c
+    
+def generate_increasing_data(n, m, ra):
+    rng = np.random
+    rng.seed(30)
+    
+    # weights
+    w = rng.randint(1, ra, size=n)
+    # profits
+    p = rng.randint(1, ra, size=n)
+    # capacities
+    c = []
+    lower_index = 0
+    upper_index = 1
+    step = 3
+    for i in range(m):
+        if i < m - 1:
+            c.append(w[lower_index:upper_index].sum())
+            lower_index = upper_index
+            upper_index = upper_index + step
+            step = step + 2
+        else:
+            c.append(w[lower_index:].sum())
+    
+    print(c)
+    print(sum(c), sum(w))
     return p, w, c
 
 
@@ -67,11 +111,11 @@ def write_csv(f, z, t):
 
 
 def main():
-    n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    m = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+    n = [10, 20, 30, 40, 50]
+    m = [2, 3, 4, 5]
+    
     parent_dir = os.getcwd()
-    path = os.path.join(parent_dir, 'output_trend')
+    path = os.path.join(parent_dir, 'output_dissimilar')
     if os.path.exists(path) and os.path.isdir(path):
         shutil.rmtree(path)
 
@@ -84,7 +128,7 @@ def main():
             n_dir = os.path.join(m_dir, str(j))
             os.mkdir(n_dir)
             os.chdir(n_dir)
-            p, w, c = generate_random_data(j, i, j)
+            p, w, c = generate_dissimilar_data(j, i, j)
                 
             start = time.perf_counter()
             z, x = heuristic(p, w, c)
@@ -96,7 +140,56 @@ def main():
             t = round(time.perf_counter() - start, 5)
             write_csv('exact', z, t)
 
+    
+    path = os.path.join(parent_dir, 'output_similar')
+    if os.path.exists(path) and os.path.isdir(path):
+        shutil.rmtree(path)
 
+    os.mkdir(path)
+
+    for i in m:
+        m_dir = os.path.join(path, str(i))
+        os.mkdir(m_dir)
+        for j in n:
+            n_dir = os.path.join(m_dir, str(j))
+            os.mkdir(n_dir)
+            os.chdir(n_dir)
+            p, w, c = generate_dissimilar_data(j, i, j)
+                
+            start = time.perf_counter()
+            z, x = heuristic(p, w, c)
+            t = round(time.perf_counter() - start, 5)
+            write_csv('greedy', z, t)
+
+            start = time.perf_counter()
+            z, x, _, _ = mtm(p, w, c)
+            t = round(time.perf_counter() - start, 5)
+            write_csv('exact', z, t)
+
+    path = os.path.join(parent_dir, 'output_increasing')
+    if os.path.exists(path) and os.path.isdir(path):
+        shutil.rmtree(path)
+
+    os.mkdir(path)
+
+    for i in m:
+        m_dir = os.path.join(path, str(i))
+        os.mkdir(m_dir)
+        for j in n:
+            n_dir = os.path.join(m_dir, str(j))
+            os.mkdir(n_dir)
+            os.chdir(n_dir)
+            p, w, c = generate_dissimilar_data(j, i, j)
+                
+            start = time.perf_counter()
+            z, x = heuristic(p, w, c)
+            t = round(time.perf_counter() - start, 5)
+            write_csv('greedy', z, t)
+
+            start = time.perf_counter()
+            z, x, _, _ = mtm(p, w, c)
+            t = round(time.perf_counter() - start, 5)
+            write_csv('exact', z, t)
 
 if __name__ == '__main__':
     main()
--- python/tests/format.py
+++ python/tests/format.py
@@ -8,30 +8,44 @@ def format_all_csv(n, m):
     path = os.path.join(parent_dir, outputDir)
     os.chdir(path)
 
-    total_profit_g = []
-    time_spent_g = []
-    total_profit_e = []
-    time_spent_e = []
-
     profit_greedy = np.zeros((10, 10))
     profit_exact = np.zeros((10, 10))
     time_greedy = np.zeros((10, 10))
     time_exact = np.zeros((10, 10))
 
     for i in m:
-        m_dir = os.path.join(path, str(i))
+        m_dir = os.path.join(path, str(i+1))
         for j in n:
-            n_dir = os.path.join(m_dir, str(j))
+            n_dir = os.path.join(m_dir, str((j+1)*10))
             os.chdir(n_dir)
             df1 = pd.read_csv('greedy_result.csv', header=0)
             df2 = pd.read_csv('exact_result.csv', header=0)
             
-            total_profit_g.append(df1['total'][0])
-            time_spent_g.append(df1['time'][0])
-            total_profit_e.append(df2['total'][0])
-            time_spent_e.append(df2['time'][0])
+            profit_greedy[i, j] = df1['total'][0]
+            time_greedy[i, j] = df1['time'][0]
+            profit_exact[i, j] = df2['total'][0]
+            time_exact[i, j] = df2['time'][0]
+        
+    df_pg = pd.DataFrame(data=profit_greedy[0:, 0:],
+                         index=list(range(1, 10)),
+                         columns=list(range(10, 110, 10)))
+    
+    df_tg = pd.DataFrame(data=time_greedy[0:, 0:],
+                         index=list(range(1, 10)),
+                         columns=list(range(10, 110, 10)))
 
+    df_pe = pd.DataFrame(data=profit_exact[0:, 0:],
+                         index=list(range(1, 10)),
+                         columns=list(range(10, 110, 10)))
 
+    df_te = pd.DataFrame(data=time_exact[0:, 0:],
+                         index=list(range(1, 10)),
+                         columns=list(range(10, 110, 10)))
 
-    # os.chdir(parent_dir)
-    # df.to_csv('result.csv')
+    df_pg.to_csv('profit_greedy.csv')
+    df_tg.to_csv('time_greed.csv')
+    df_pe.to_csv('profit_exact.csv')
+    df_te.to_csv('time_exact.csv')
+
+if __name__ == '__main__':
+    format_all_csv(10, 9)
